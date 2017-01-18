c1ccfea344e547934d6f2b933a417294d1af80f4
ernestmachado <ernestmachado@rediffmail.com>
2017-01-18 21:26:59 +0530

    Assignment8_2
    Assignment8_1

---
+++ Assignment8.2/src/UserDefineException/CheckAge.java
@@ -0,0 +1,9 @@
+package UserDefineException;
+
+//Define a method to check age and accordingly throw the exception
+public class CheckAge {
+	public void setAge(int age) throws NegativeAgeException {
+		if (age < 0)
+			throw new NegativeAgeException();
+	}
+}
